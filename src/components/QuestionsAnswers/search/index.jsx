46f3cf666b429e0cd4d80efef62d64c1fac60b43
Eric Grochowski <egrochowski42@gmail.com>
2021-11-02 22:10:49 -0400

    Convert search to a stateless component

--- src/components/QuestionsAnswers/search/index.jsx
+++ src/components/QuestionsAnswers/search/index.jsx
@@ -1,31 +1,13 @@
 import React from 'react';
 
-class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: ''
-    }
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    console.log(event.target.value);
-    this.setState({ query: event.target.value });
-  }
-
-  render() {
-    return (
-      <div className="questions-search" >
-        <input
-          type="search"
-          placeholder="Have a question? Search for the answers..."
-          onChange={this.handleChange}
-        />
-        <button>Submit</button>
-      </div>
-    );
-  }
-}
+const Search = (props) => (
+  <div className="questions-search" >
+    <input
+      type="search"
+      placeholder="Have a question? Search for the answers..."
+      onChange={props.onChange}
+    />
+  </div>
+);
 
 export default Search;
