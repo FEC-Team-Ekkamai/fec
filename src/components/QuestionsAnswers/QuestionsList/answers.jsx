d1c9c365c7b9a3ad4db9017f851710006a4361d3
Eric Grochowski <egrochowski42@gmail.com>
2021-11-03 17:19:14 -0400

    Add labels for questions and answers and bold text if answerer is the seller

--- src/components/QuestionsAnswers/QuestionsList/answers.jsx
+++ src/components/QuestionsAnswers/QuestionsList/answers.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import moment from 'moment';
 
+var isSeller = (name) => {
+  return name.toLowerCase() === 'seller';
+}
+
 const Answers = (props) => (
   <li>
     <div className="answer-header">
-      {props.answer.body}
+      A: {props.answer.body}
     </div>
     <div className="answer-footer">
-      <span>by {props.answer.answerer_name}, {moment(props.answer.date).format('MMMM D, YYYY')} |  </span>
+      <span>{isSeller(props.answer.answerer_name)
+        ? <b>{props.answer.answerer_name}</b>
+        : <p>{props.answer.answerer_name}</p>}
+      </span>
+      <span>  {moment(props.answer.date).format('MMMM D, YYYY')} |  </span>
       <span>Helpful? <u>Yes</u> ({props.answer.helpfulness})  |  </span>
       <span><u>Report</u></span>
     </div>
