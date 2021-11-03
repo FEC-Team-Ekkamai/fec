d089f93be4c903427a8dd8beda071d55bf062ea8
Eric Grochowski <egrochowski42@gmail.com>
2021-11-02 20:54:59 -0400

    Implement search filter for questions

--- src/components/QuestionsAnswers/index.jsx
+++ src/components/QuestionsAnswers/index.jsx
@@ -8,25 +8,47 @@ class QuestionsView extends React.Component {
     super(props);
     this.state = {
       questions: [],
+      displayedQuestions: [],
       answers: []
     }
+    this.filterQuestions = this.filterQuestions.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     axios.get(`/api/products/questions/?product_id=${this.props.productId}`)
-      .then(questions => {
-        this.setState({ questions: questions.data.results })
+    .then(questions => {
+      this.setState({ questions: questions.data.results });
+      this.filterQuestions('');
+    })
+    .catch(console.error)
+  }
+
+  handleChange(event) {
+    this.filterQuestions(event.target.value);
+  }
+
+  filterQuestions(query) {
+    this.setState({
+      displayedQuestions: this.state.questions.filter(question => {
+        return question.question_body.toLowerCase().includes(query.toLowerCase());
       })
-      .catch(console.error)
+    })
   }
 
   render() {
     return (
-      <div className="qa-container">
-        <h3>Questions!</h3>
-        <Search />
-        <QuestionsList questions={this.state.questions} />
-      </div>
+      <>
+        <div className="qa-container">
+          <h3>Questions &amp; Answers</h3>
+          <Search onChange={this.handleChange} />
+          <QuestionsList questions={this.state.displayedQuestions}/>
+        </div>
+        <div className="questions-footer">
+          <button className="add-question">Add a Question</button>
+          <button className="view-more-questions">More Answered Questions</button>
+        </div>
+      </>
     );
   }
 }
