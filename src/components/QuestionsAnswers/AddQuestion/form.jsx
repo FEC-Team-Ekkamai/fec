223e5e56b7dff414f05e19b3e7a71df1f9ff5b13
Eric Grochowski <egrochowski42@gmail.com>
2021-11-05 20:52:03 -0400

    Convert form to a state component and create post request

--- src/components/QuestionsAnswers/AddQuestion/form.jsx
+++ src/components/QuestionsAnswers/AddQuestion/form.jsx
@@ -7,15 +7,34 @@ const QuestionBody = styled.textarea`
   resize: none;
 `;
 
-const Form = (props) => (
-  <form onSubmit={props.onSubmit}>
-    <label htmlFor="name">Name: </label>
-    <input type="text" /><br />
-    <label htmlFor="email">e-mail: </label>
-    <input type="text" /><br />
-    <label htmlFor="question-input">What is your question? </label>
-    <QuestionBody></QuestionBody>
-  </form>
-);
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      body: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
+  handleChange(event) {
+    let name = event.target.name;
+    this.setState({ [name]: event.target.value });
+  }
+
+  render() {
+    return (
+      <form onSubmit={e => this.props.onSubmit(e, this.state)}>
+        <label htmlFor="name">Name: </label>
+        <input name="name" type="text" onChange={this.handleChange} /><br />
+        <label htmlFor="email">e-mail: </label>
+        <input name="email" type="text" onChange={this.handleChange} /><br />
+        <label htmlFor="question-input">What is your question? </label>
+        <QuestionBody name="body" onChange={this.handleChange} />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
 export default Form;
