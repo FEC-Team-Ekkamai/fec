4094ea19bccf38683730299f86f3c238d684f251
Eric Grochowski <egrochowski42@gmail.com>
2021-11-08 14:13:48 -0500

    Add scrollable feature to questions list

--- src/components/QuestionsAnswers/QuestionsList/questionsList.jsx
+++ src/components/QuestionsAnswers/QuestionsList/questionsList.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import Questions from './questions.jsx';
+import styled, { css } from 'styled-components';
 
-const QuestionsList = (props) => (
-  <div className="questions-view">
-     <div className="questions-list">
-      <ul>
-        {props.questions.map((question, index) => (
-          index < props.numQuestionsDisplayed
-            ? <Questions
-                question={question}
-                key={question.question_id}
-              />
-            : null
-        ))}
-      </ul>
+const QuestionsList = (props) => {
+
+  const List = styled.div`
+    height: 300px;
+    overflow-y: auto;
+  `;
+
+  return (
+    <div className="questions-view">
+      <List className="questions-list">
+        <ul>
+          {props.questions.map((question, index) => (
+            index < props.numQuestionsDisplayed
+              ? <Questions
+                  question={question}
+                  numAnswersDisplayed={props.numAnswersDisplayed}
+                  viewMoreAnswers={props.viewMoreAnswers}
+                  key={question.question_id}
+                />
+              : null
+          ))}
+        </ul>
+      </List>
     </div>
-  </div>
-);
+  );
+}
 
 export default QuestionsList;
