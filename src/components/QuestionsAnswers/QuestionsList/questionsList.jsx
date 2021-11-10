60877501e030a619e06179cccf970eb0cb7fd729
Eric Grochowski <egrochowski42@gmail.com>
2021-11-09 21:21:01 -0500

    Resolve warning message

--- src/components/QuestionsAnswers/QuestionsList/questionsList.jsx
+++ src/components/QuestionsAnswers/QuestionsList/questionsList.jsx
@@ -2,30 +2,25 @@ import React from 'react';
 import Questions from './questions.jsx';
 import styled, { css } from 'styled-components';
 
+const List = styled.div`
+  height: 300px;
+  overflow-y: auto;
+`;
+
 const QuestionsList = (props) => {
-
-  const List = styled.div`
-    height: 300px;
-    overflow-y: auto;
-  `;
-
   return (
-    <div className="questions-view">
-      <List className="questions-list">
-        <ul>
-          {props.questions.map((question, index) => (
-            index < props.numQuestionsDisplayed
-              ? <Questions
-                  question={question}
-                  numAnswersDisplayed={props.numAnswersDisplayed}
-                  viewMoreAnswers={props.viewMoreAnswers}
-                  key={question.question_id}
-                />
-              : null
-          ))}
-        </ul>
-      </List>
-    </div>
+    <List className="questions-list">
+      <ul>
+        {props.questions.slice(0, props.numQuestionsDisplayed).map(question => (
+          <Questions
+            question={question}
+            numAnswersDisplayed={props.numAnswersDisplayed}
+            viewMoreAnswers={props.viewMoreAnswers}
+            key={question.question_id}
+          />
+        ))}
+      </ul>
+    </List>
   );
 }
 
