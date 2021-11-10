94498a02f31639dde551a310528009edcf5a1efb
Eric Grochowski <egrochowski42@gmail.com>
2021-11-09 21:08:46 -0500

    Refactor and create generic modal

---
+++ src/components/QuestionsAnswers/modal.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const ModalContainer  = styled.div`
+  background: white;
+  border: 1px solid #d0cccc;
+  box-shadow: 0 5px 8px 0 rgba(0,0,0,0.2), 0 7px 20px 0 rgba(0,0,0,0.17);
+  margin: 100px auto 0;
+  transition: all .8s;
+  width: 60%;
+  border-radius: 10px;
+`;
+
+const Body = styled.div`
+  padding: 10px 15px;
+  text-align: center;
+  height:200px;
+`;
+
+const Header = styled.div`
+  background: #e2e2e2;
+  height: 40px;
+  line-height: 40px;
+  padding: 5px 20px;
+  text-align: right;
+  margin: 0;
+  padding: 0;
+`;
+
+const Footer = styled.div`
+  background: #e2e2e2;
+  height: 35px;
+  padding: 15px;
+`;
+
+const Button = styled.button`
+  color: black;
+  cursor: pointer;
+  float: right;
+  margin: 0;
+`;
+
+const Modal = (props) => {
+  if (!props.view) {
+    return null;
+  }
+  return (
+    <ModalContainer className="modal-container">
+      <div className="container">
+        <Header>Submit a {props.headerText}</Header>
+        <Body className="body">{props.children}</Body>
+        <Footer>
+          <Button onClick={props.onClose}>Close</Button>
+        </Footer>
+      </div>
+    </ModalContainer>
+  )
+};
+
+export default Modal;
