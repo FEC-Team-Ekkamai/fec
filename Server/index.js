4be80ee7eda8425d443338e4f9078362e9a6b077
Eric Grochowski <egrochowski42@gmail.com>
2021-11-10 02:08:47 -0500

    Add PUT route to mark an answer as helpful

--- Server/index.js
+++ Server/index.js
@@ -209,6 +209,22 @@ app.put('/api/products/questions/helpful', (req, res) => {
     });
 });
 
+app.put('/api/products/answer/helpful', (req, res) => {
+  let questionOptions = {
+    method: 'POST',
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions/${req.body.answer_id}/helpful`,
+    headers: api,
+    data: req.body // { question_id}
+  };
+  axios(questionOptions)
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
+});
+
 const port = 3000;
 
 app.listen(port, () => {
