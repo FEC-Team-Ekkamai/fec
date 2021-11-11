b7ce865dedf5aec772a3d5bc43803e59c4f9e1b3
Eric Grochowski <egrochowski42@gmail.com>
2021-11-11 11:58:01 -0500

    Add helpful and report functionality

--- src/components/QuestionsAnswers/QuestionsList/answers.jsx
+++ src/components/QuestionsAnswers/QuestionsList/answers.jsx
@@ -1,28 +1,47 @@
 import React from 'react';
 import moment from 'moment';
+import axios from 'axios';
 
-var isSeller = (name) => {
-  return name.toLowerCase() === 'seller';
-}
+const Answers = (props) => {
 
-const Answers = (props) => (
-  <li>
-    <div className="answer-container">
-      <div className="answer-identifier">A:</div>
-      <div className="answer-body">
-        <div className="answer-text">{props.answer.body}</div>
-        <div className="answer-footer">
-          <span>{isSeller(props.answer.answerer_name)
-            ? <b>{props.answer.answerer_name}</b>
-            : <p>{props.answer.answerer_name}</p>}
-          </span>
-          <span>  {moment(props.answer.date).format('MMMM D, YYYY')} |  </span>
-          <span>Helpful? <u>Yes</u> ({props.answer.helpfulness})  |  </span>
-          <span><u>Report</u></span>
-        </div>
-        </div>
-    </div>
-  </li>
-);
+  const id = {answer_id: props.answer.id}
+
+  let handleHelpfulClick = (event) => {
+    axios.put('/api/products/answers/helpful', id)
+      .then(() => console.log('success'))
+      .catch(console.error);
+  };
+
+  let handleReport = (event) => {
+    axios.put('/api/products/answers/report', id)
+      .then(() => console.log('success'))
+      .catch(console.error);
+  };
+
+  let isSeller = (name) => {
+    return name.toLowerCase() === 'seller';
+  };
+
+  return (
+    <li>
+      <div className="answer-container">
+        <div className="answer-body">
+          <div className="answer-text">{props.answer.body}</div>
+          <div className="answer-footer">
+            <div className="image-container">{renderImages()}</div>
+            <span>
+              {isSeller(props.answer.answerer_name)
+              ? <b>{props.answer.answerer_name}</b>
+              : <p>{props.answer.answerer_name}</p>}
+            </span>
+            <span>  {moment(props.answer.date).format('MMMM D, YYYY')} |  </span>
+            <span onClick={handleHelpfulClick}>Helpful? <u>Yes</u> ({props.answer.helpfulness})  |  </span>
+            <span><u onClick={handleReport}>Report</u></span>
+          </div>
+          </div>
+      </div>
+    </li>
+  );
+};
 
 export default Answers;
