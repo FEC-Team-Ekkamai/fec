954ec739ba60c509ea07ca0a32638d2ca95d54db
Mitchel Dupuie <mtdupuie@gmail.com>
2021-11-12 14:41:57 -0500

    Added social logos under add to cart

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,18 @@ module.exports = {
         test: /.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
-      }
+      },
+      {
+        test: /\.(jpe?g|gif|png|svg)$/i,
+        use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 10000
+          }
+        }
+      ]
+    }
     ]
   },
 
